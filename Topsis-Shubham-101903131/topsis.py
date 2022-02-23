19cc2322f648029ed9d5230a1aaa951fd289ae4a
greatgabbar <strivedi_be19@thapar.edu>
2022-02-24 02:22:52 +0530

    publisihing package

---
+++ Topsis-Shubham-101903131/topsis.py
@@ -0,0 +1,158 @@
+# Shubham Trivedi
+# 101903131
+# 3COE5
+
+import math
+import sys
+import pandas as pd
+import numpy as np
+
+
+def topsis():
+    if len(sys.argv) != 4:
+        print('NOT ENOUGH ARG')
+        sys.exit()
+
+    try:
+        filename = sys.argv[1]
+        weights = sys.argv[2]
+        impact = sys.argv[3]
+        weights = weights.split(",")
+        weights = [float(x) for x in weights]
+        impact = impact.split(",")
+    except:
+        print("INVALID INOUT VALUES")
+        sys.exit()
+    try:
+        df = pd.read_csv(filename)
+    except:
+        print("File Not Found")
+        sys.exit()
+    # print(df)
+    dff = df.iloc[:, 1:].copy(deep=True)
+    # print(dff)
+    # arr = np.zeros((r, c-1))
+    # print(arr)
+
+    # for i in range(0, r):
+    #     arr[i]=arr[i] + df[]
+    #     print(arr[i])
+
+    # Array of rms value of all column
+    rms = {}
+    r = len(dff)
+    c = len(dff.columns)
+    if c < 3:
+        print("Input file must contain three or more columns")
+        sys.exit()
+    if len(weights) != c:
+        print("INSUFFICIENT WEIGHTS")
+        sys.exit()
+    if len(impact) != c:
+        print("INSUFFICIENT IMPACT VALUES")
+        sys.exit()
+    # print(dff.columns.dtype)
+    dff.astype(float)
+    print(df.applymap(np.isreal))
+    for i in dff:
+        dff[i].astype(float)
+
+    for i in dff:
+        sum = 0
+        for j in dff[i]:
+            # print(dff[i][j])
+            sum = sum+j*j
+        rms[i] = sum
+
+    for i in rms:
+        rms[i] = math.sqrt(rms[i])
+
+    # print(rms)
+
+    # normalizing values
+
+    for i in dff:
+        for j in range(0, r):
+            # print(dff[i][j], rms[i], dff[i][j]/rms[i])
+            dff[i][j] = float(dff[i][j]/rms[i])
+
+    # print(dff)
+    # multiplying by weights
+
+    index = 0
+    for i in dff:
+        w = weights[index]
+        for j in range(0, r):
+            print(dff[i][j])
+            dff[i][j] = w*dff[i][j]
+        index = index+1
+
+    # print(dff)
+
+    ideal_best = {}
+    ideal_worst = {}
+
+    index = 0
+    for i in dff:
+        if impact[index] == "+":
+            ideal_best[i] = dff[i].max()
+            ideal_worst[i] = dff[i].min()
+        if impact[index] == "-":
+            ideal_best[i] = dff[i].min()
+            ideal_worst[i] = dff[i].max()
+        index = index+1
+
+    # print(ideal_best)
+
+    # print(ideal_worst)
+
+    edistance_positive = []
+    edistance_negative = []
+
+    for i in range(0, r):
+        temp_p = 0
+        temp_n = 0
+        for j in dff:
+            best = ideal_best[j]
+            worst = ideal_worst[j]
+            # print(dff[j][i], best, worst)
+            temp_p = temp_p+(dff[j][i]-best)*(dff[j][i]-best)
+            temp_n = temp_n+(dff[j][i]-worst)*(dff[j][i]-worst)
+        edistance_positive.append(math.sqrt(temp_p))
+        edistance_negative.append(math.sqrt(temp_n))
+
+    # print(edistance_positive)
+    # print(edistance_negative)
+
+    # topsis score
+    pscore = []
+
+    for i in range(0, r):
+        pscore.append(
+            edistance_negative[i]/(edistance_negative[i]+edistance_positive[i]))
+
+    # print(pscore)
+
+    # finding rank
+    # array = np.array(pscore)
+    # order = array.argsort()
+    # ranks = order.argsort()
+
+    rank = {}
+
+    pscore_sorted = np.sort(pscore)[::-1]
+
+    for i in range(len(pscore_sorted)):
+        rank[pscore_sorted[i]] = i+1
+
+    # print(rank)
+    ranks = []
+
+    for i in pscore:
+        gg = rank[i]
+        ranks.append(gg)
+
+    print(ranks)
+    df["TScore"] = pd.Series(pscore)
+    df["Rank"] = pd.Series(ranks)
+    print(df)
---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+    # How you named your package folder (MyLib)
+    name='Topsis-Shubham-101903131',
+    packages=['Topsis-Shubham-101903131'],   # Chose the same as "name"
+    version='0.0.1',      # Start with a small number and increase it with every change you make
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license='MIT',
+    # Give a short description about your library
+    description='Simple Topsis Alogrithum ',
+    author='Shubham Trivedi',                   # Type in your name
+    author_email='strivedi_b19@thapar.edu',      # Type in your E-Mail
+    # Provide either the link to your github or to your website
+    url='https://github.com/user/reponame',
+    # I explain this later on
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    # Keywords that define your package best
+    keywords=['topsis', 'python', 'Selection'],
+    install_requires=[            # I get to this in a second
+        'numpy',
+        'pandas',
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
